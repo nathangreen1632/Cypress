049db109ad9ec2ee27060836afb4a60eb5ec4e43
Nathan Green <ngreen1632@gmail.com>
2025-03-10 19:04:33 -0500

    Refactor router initialization to use explicit typing
    Updated the `router` definition to include an explicit `Router` type. This enhances type safety and aligns with TypeScript best practices.

--- server/src/routes/api/questionRoutes.ts
+++ server/src/routes/api/questionRoutes.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-const router = express.Router();
+import express, {Router} from 'express';
+const router: Router = express.Router();
 import {
   getRandomQuestions
 } from '../../controllers/questionController.js';
