2d2e15fa2911bdc80038cbf99cfa86f0f4fdc57c
Nathan Green <ngreen1632@gmail.com>
2025-03-11 10:58:13 -0500

    Refactor questionController by removing outdated comments
    Removed unused comments in questionController for improved clarity. These changes enhance the readability and maintainability of the codebase. No functionality has been altered.

--- server/src/controllers/questionController.ts
+++ server/src/controllers/questionController.ts
@@ -1,8 +1,7 @@
 import type { Request, Response } from 'express';
-// import question model
 import { IQuestion, Question} from '../models/Question.js';
 
-// gets a set of random questions
+
 export const getRandomQuestions = async (_req: Request, res: Response) => {
   try {
     const questions = await Question.aggregate<IQuestion>([
